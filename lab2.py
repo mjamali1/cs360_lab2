e2d1fc8a7a605db6a1e4b1b208d94a85f24d098c
Mariyah Jamali <mjamali@usc.edu>
2025-10-14 21:41:10 -0700

    pls pls pls

--- lab2.py
+++ lab2.py
@@ -1,6 +1,7 @@
 # you can add imports but you should not rely on libraries that are not already provided in "requirements.txt #
 from collections import deque
 from heapq import heappush, heappop
+import heapq
 import numpy as np
 
 # for weighted a star, function and self-testing
@@ -72,19 +73,37 @@ def apply_sequence(stack, sequence):
 def a_star_search(stack):
     
 #     # --- v ADD YOUR CODE HERE v --- #
-    
-    # initialize
-    # stack is the initial gistate
-    start_key = str(stack)
-    # open set is the list of nodes to explore
-    open_set = [stack]
-    # flip sequence is the list of flips to get to the goal state
     flip_sequence = []
-    # dictionary to track each state to the sequence of flips used to reach it
-    node_path = {str(stack): []}
+    
+    # # initialize
+    # # stack is the initial gistate
+    # start_key = str(stack)
+    # # open set is the list of nodes to explore
+    # open_set = [stack]
+    # # flip sequence is the list of flips to get to the goal state
+    # flip_sequence = []
+    # # dictionary to track each state to the sequence of flips used to reach it
+    # node_path = {str(stack): []}
+    # # set of visited nodes
+    # closed_set = set()
+
+    # heap implementation for efficiency
+    # using a heap for open set to optimize retrieval of lowest f score
+    
+    # stack in string version
+    start_key = str(stack)
+    # PQ for open set
+    open_heap = []  
+    # start node into open_heap
+    heapq.heappush(open_heap, (0, 0, stack))  # (f_score, counter, node)
+    # to track each state to the sequence of flips used to reach it
+    node_path = {start_key: []}
     # set of visited nodes
     closed_set = set()
+    # to break ties in heap
+    counter = 0  
     
+    # replacer for helper function, inline heurisitic
     h0=0
     for i in range(stack.num_books):
         if stack.order[i] != i or stack.orientations[i] != 1:
@@ -100,38 +119,41 @@ def a_star_search(stack):
     f = {str(stack): g[str(stack)] + h[str(stack)]}
 
     # while there are nodes to explore
-    while open_set:
+    while open_heap:
 
-        # get node with lowest f value, keeping curr as string and int
-        # line 100 disclaimer - used outside sources for guidance
-        # my original line and subsequent attempts would not compile
-        curr = min(open_set, key=lambda s: f.get(str(s), float('inf')))
+        # get node in open set with lowest f score (and remove from open), set as current
+        f_curr, _, curr = heapq.heappop(open_heap)
         curr_key = str(curr)
 
+        # if curr_key not in node_path:
         if curr_key not in node_path:
-            node_path[curr_key] = []  # at worst, treat as start
+            # keep current key empty
+            node_path[curr_key] = []
+        # if curr_key not in g:
         if curr_key not in g:
+            # cost from source to curr is length of path to curr
             g[curr_key] = len(node_path[curr_key])
-            # inline heuristic for curr (no helper)
-            h_curr = 0
+            
+            # inline heuristic
+            h_neighbor = 0
             for j in range(curr.num_books):
-                if curr.order[j] != j or curr.orientations[j] != 1:
-                    h_curr += 1
-            h[curr_key] = h_curr
+                if (curr.order[j] != j or curr.orientations[j] != 1):
+                    h_neighbor += 1
+
+            # calculate h and f for curr
+            h[curr_key] = h_neighbor
             f[curr_key] = g[curr_key] + h[curr_key]
 
         # if current node is goal state, return flip sequence
         if curr.check_ordered():
-            flip_sequence = node_path[curr_key]
-            return flip_sequence
+            return node_path[curr_key]
         
         # mark node as visitied
-        open_set.remove(curr)
         closed_set.add(curr_key)
-
+        
         # explore neighbors
-        # for each neighbor of current node
-        for i in range (1, curr.num_books + 1):
+        # for each flip position, copy the stack, do the flip, get the string key
+        for i in range(1, curr.num_books + 1):
             neighbor = curr.copy()
             neighbor.flip_stack(i)
             neighbor_key = str(neighbor)
@@ -140,47 +162,30 @@ def a_star_search(stack):
             if neighbor_key in closed_set:
                 continue
             
-            # cost from source to neighbor
+            # new cost to reach neighbor
             tentative_g = g[curr_key] + 1
-
-            node_path[neighbor_key] = node_path[curr_key] + [i]
-
+            
             # inline heuristic
             h_neighbor = 0
-            for j in range(neighbor.num_books):
-                if neighbor.order[j] != j or neighbor.orientations[j] != 1:
+            for j in range(curr.num_books):
+                if (curr.order[j] != j or curr.orientations[j] != 1):
                     h_neighbor += 1
 
-            g[neighbor_key] = g[curr_key] + 1
+            # caluclate g, h, f for neighbor
+            # record the path
+            g[neighbor_key] = tentative_g
             h[neighbor_key] = h_neighbor
-            f[neighbor_key] = g[neighbor_key] + h[neighbor_key]
+            f[neighbor_key] = tentative_g + h_neighbor
+            node_path[neighbor_key] = node_path[curr_key] + [i]
+            counter += 1 # increment counter for tie-breaking in heap
+            # push with total cost and total tie-breaker
+            heapq.heappush(open_heap, (f[neighbor_key], counter, neighbor))
 
-            # if neighbor is not in open set, add it
-            if neighbor not in open_set:
-                open_set.append(neighbor)
-            # else if tentative g is greater than g of neighbor, skip
-            elif tentative_g >= g.get(str(neighbor), float('inf')):
-                continue
-        
-        # replacement for helper function
-        h_neighbor = 0
-        for j in range(neighbor.num_books):
-            if neighbor.order[j] != j or neighbor.orientations[j] != 1:
-                h_neighbor += 1
-
-        # record f score to neighbor -- str(neigher) or neighbor_key
-        # g[neighbor_key] = tentative_g
-        # h[neighbor_key] = h_neighbor
-        # f[neighbor_key] = g[neighbor_key] + h[neighbor_key]
-
-        # # record path to neighbor
-        # node_path[neighbor_key] = node_path[curr_key] + [i]
-
-    # return least cost path from start to end
+    return flip_sequence
     # return flip_sequence
 
     # if no solution found
-    return []
+    # return []
 #     # ---------------------------- #
 
 
